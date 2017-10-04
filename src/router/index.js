3d4825855e5e028512b0d7e628f0564e63cfca69
ray <devwanglei@outlook.com>
2017-10-04 13:42:18 +0800

    my first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import goodslist from '@/views/goodslist'
+import title from '@/views/title'
+import image from '@/views/image'
+import Cart from '@/views/Cart'
 
 Vue.use(Router)
 
 export default new Router({
+  // mode:'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'goodslist',
+      components: {
+        default: goodslist,
+        title: title,
+        img: image
+      }
+    }, {
+      path: '/cart/:cartId',
+
+      name: 'cart',
+      component: Cart
     }
+
   ]
 })
---
+++ src/views/image.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    这里是商品图片
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+
+  export default {
+    data() {
+      return {
+        msg: 'hello vue'
+      }
+    }
+  }
+</script>
